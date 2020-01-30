45b1d661f769bf83ac6377b332b7157bf6db43d9
Jakub <jakubmichalik98-98@wp.pl>
2020-01-30 16:13:37 +0100

    creating table

---
+++ creating_table.py
@@ -0,0 +1,21 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, create_engine, Integer, String, Float, DateTime
+
+Base = declarative_base()
+
+
+class Expenses(Base):
+    __tablename__ = "expenses"
+    id = Column(Integer, primary_key=True, name="Id")
+    product = Column(String(30), name="Product")
+    price = Column(Float, name="Price")
+    date_added = Column(DateTime, name="DateAdded")
+
+    def __repr__(self):
+        return f"Expense: {self.id}, product {self.product}, price: {self.price}, date added: {self.date_added}"
+
+
+if __name__ == '__main__':
+    engine = create_engine("sqlite:///:memory:", echo=True)
+    Base.metadata.create_all(engine)
+
